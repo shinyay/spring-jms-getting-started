b2b0d893aefee912524ebbcdd65003d312eeaa13
shinyay <shinya.com@gmail.com>
2020-08-18 22:01:37 +0900

    Add Active MQ Broker dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-activemq")
+	implementation("org.apache.activemq:activemq-broker")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
