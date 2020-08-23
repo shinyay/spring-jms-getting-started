a09f1821f72479e3a314fc5fb475639e71dd272a
shinyay <shinya.com@gmail.com>
2020-08-23 22:08:31 +0900

    Add Listener for JMS Message

--- src/main/kotlin/com/google/shinyay/listener/Receiver.kt
+++ src/main/kotlin/com/google/shinyay/listener/Receiver.kt
@@ -1,4 +1,13 @@
 package com.google.shinyay.listener
 
+import com.google.shinyay.model.Message
+import org.springframework.jms.annotation.JmsListener
+import org.springframework.stereotype.Component
+
+@Component
 class Receiver {
+
+    @JmsListener(destination = "messagebox", containerFactory = "myFactory")
+    fun receiveMessage(message: Message) {
+    }
 }
