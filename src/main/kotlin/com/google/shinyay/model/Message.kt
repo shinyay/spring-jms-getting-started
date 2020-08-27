34979364ad3ebf745cf9180438b1bca725b3fe53
shinyay <shinya.com@gmail.com>
2020-08-28 07:14:13 +0900

    Add default value to Data class as Default Constructor

--- src/main/kotlin/com/google/shinyay/model/Message.kt
+++ src/main/kotlin/com/google/shinyay/model/Message.kt
@@ -1,4 +1,4 @@
 package com.google.shinyay.model
 
-data class Message(val to: String,
-                   val body: String)
+data class Message(val to: String = "",
+                   val body: String = "")
