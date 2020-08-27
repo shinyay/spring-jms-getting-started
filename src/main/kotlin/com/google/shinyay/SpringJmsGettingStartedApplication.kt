bd11e2139147122118dbf41d807b73850c1da9c2
shinyay <shinya.com@gmail.com>
2020-08-27 23:34:10 +0900

    Add Connection Factory

--- src/main/kotlin/com/google/shinyay/SpringJmsGettingStartedApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringJmsGettingStartedApplication.kt
@@ -4,9 +4,16 @@ import com.google.shinyay.model.Message
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.jms.annotation.EnableJms
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory
 import org.springframework.jms.core.JmsTemplate
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter
+import org.springframework.jms.support.converter.MessageConverter
+import org.springframework.jms.support.converter.MessageType
+import javax.jms.ConnectionFactory
 
 @SpringBootApplication
 @EnableJms
@@ -18,5 +25,15 @@ fun main(args: Array<String>) {
 	val jmsTemplate = context.getBean(JmsTemplate::class.java)
 	jmsTemplate.convertAndSend("messagebox", Message("Hello", "Hello JMS"))
 }
+
+@Bean
+fun myFactory(connectionFactory: ConnectionFactory,
+			  configurer: DefaultJmsListenerContainerFactoryConfigurer): DefaultJmsListenerContainerFactory {
+	val factory = DefaultJmsListenerContainerFactory()
+	configurer.configure(factory, connectionFactory)
+	return factory
+}
+
+
 val Any.logger: Logger
 	get() = LoggerFactory.getLogger(this.javaClass)
