43fb09f7a0d76581bbd1ded5d8c6d8386066c893
shinyay <shinya.com@gmail.com>
2020-08-27 23:06:09 +0900

    Add JMS Template to send a message

--- src/main/kotlin/com/google/shinyay/SpringJmsGettingStartedApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringJmsGettingStartedApplication.kt
@@ -1,17 +1,22 @@
 package com.google.shinyay
 
+import com.google.shinyay.model.Message
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.jms.annotation.EnableJms
+import org.springframework.jms.core.JmsTemplate
 
 @SpringBootApplication
 @EnableJms
 class SpringJmsGettingStartedApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringJmsGettingStartedApplication>(*args)
+	val context =
+			runApplication<SpringJmsGettingStartedApplication>(*args)
+	val jmsTemplate = context.getBean(JmsTemplate::class.java)
+	jmsTemplate.convertAndSend("message", Message("Hello", "Hello JMS"))
 }
 val Any.logger: Logger
 	get() = LoggerFactory.getLogger(this.javaClass)
